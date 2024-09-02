7447b3585bc46f8a7c2169ff004dfb67d8b9e5de
Ansuhan <anushan559@gmail.com>
2024-09-02 19:23:21 +0530

    LogingFile

---
+++ DataBase/Logger.js
@@ -0,0 +1,23 @@
+const { createLogger, format, transports } =require("winston");
+const { combine, timestamp, json, colorize } = format;
+
+// Custom format for console logging with colors
+const consoleLogFormat = format.combine(
+format.colorize(),
+format.printf(({ level, message, timestamp }) => {
+    return `${level}: ${message}`;
+})
+);
+
+// Create a Winston logger
+exports.logger = createLogger({
+  level: "info",
+  format: combine(colorize(), timestamp(), json()),
+  transports: [
+    new transports.Console({
+      format: consoleLogFormat,
+    }),
+    new transports.File({ filename: "DataBase/app.log" }),
+  ],
+});
+
