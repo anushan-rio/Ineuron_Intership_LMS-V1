10b2c7a3c945c514a6fe13fc6ae337c519bf4b13
Ansuhan <anushan559@gmail.com>
2024-09-03 18:54:52 +0530

    Logger Config

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { PORT_NUMBER } = require('./constants');
 const express=require('express');
 const mongoose=require("mongoose");
+const morgan = require('morgan');
+const {logger}=require("./DataBase/Logger.js")
+
 const App=express();
 const bodyParser = require('body-parser') 
 const db=require("./DataBase/DbConfig").mongoURL
@@ -15,17 +18,43 @@ mongoose.connect(db,{
     .catch(()=>console.log("Database NotConnected"));
 
 //Middleware
-App.use(bodyParser.json())
+App.use(bodyParser.json());
+
+
+
+const morganFormat = ":method :url :status :response-time ms";
+App.use(
+morgan(morganFormat, {
+    stream: {
+    write: (message) => {
+        const logObject = {
+        method: message.split(" ")[0],
+        url: message.split(" ")[1],
+        status: message.split(" ")[2],
+        responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+    },
+    },
+})
+);
 
 
 //RouterPath
 const AuthRoute=require("./Routers/Auth.router");
 const UserRoute=require("./Routers/User.router");
 const CategoryRoute=require("./Routers/Category.router");
+const BooksStatusRoute=require("./Routers/BookStatus.router");
+const BookCondtionRoute=require("./Routers/BooksCondtion.router");
+const BooksRoute=require("./Routers/Books.router");
+
 //Api Calling
 App.use('/api/v1',AuthRoute);
 App.use('/api/v1',UserRoute);
 App.use('/api/v1',CategoryRoute);
+App.use('/api/v1',BooksStatusRoute);
+App.use('/api/v1',BookCondtionRoute);
+App.use('/api/v1',BooksRoute);
 
 
 App.listen(Port,()=>console.log(`app is conected to the ${Port}`))
