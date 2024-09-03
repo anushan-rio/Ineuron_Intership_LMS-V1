0d7d3c859d4e8b2d6ec9a5d3e3a39339398d9ce0
Ansuhan <anushan559@gmail.com>
2024-09-03 18:53:20 +0530

    Books Model

---
+++ Models/Books.model.js
@@ -0,0 +1,59 @@
+const mongosse=require('mongoose');
+const {ObjectId}=mongosse.Schema;
+
+const Books=new mongosse.Schema({
+    Title:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    Author:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    Publisher:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    Edition:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    Price:{
+        type:Number,
+        require:true,
+        trim:true
+    },
+    Copies:{
+        type:Number,
+        require:true,
+        trim:true
+    },
+    BookCategory:{
+        type: ObjectId,
+        ref:"Category"
+    },
+    User:{
+        type: ObjectId,
+        ref:"User"
+    },
+    BookStatus:{
+        type: ObjectId,
+        ref:"BooksStatus"
+    },
+    BookCondtion:{
+        type: ObjectId,
+        ref:"BooksCondtion"
+    } ,
+    BookCode:{
+        type:String,
+        trim:true
+    }
+
+},{timestamps:true})
+
+module.exports=mongosse.model("Book",Books);
+
